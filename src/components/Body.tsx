e2f73ca676d2a2e2284a049f3d31a1efa3b414f2
Abigail Curiel <a01655892@itesm.mx>
2024-02-29 16:20:09 -0600

    Changed classes for a better collective card display.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -68,8 +68,6 @@ export default function Body() {
     return (
         <div className='main-body p-4'>
             <InputPokemon />
-            <h1 className="text-3xl font-bold text-center mb-8 mt-10">Pokémons</h1>
-
             <div className="pokemon-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
 
                 {pokemons.map(pokemon => (
