1552b855687e9936b5e6315d9ad539d29270ebfa
Abigail Curiel <a01655892@itesm.mx>
2024-03-01 01:02:20 -0600

    Made the info card more responsive.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Body from './components/Body'
-import InfoCard from './components/InfoCard';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar home="#" links={{'Wikipedia': 'https://en.wikipedia.org/wiki/Pok%C3%A9mon', 'Api': 'https://pokeapi.co/', 'Random': 'random'}}></Navbar>
-
-      <InfoCard id="1" imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" name="Pikachu"></InfoCard>
       <Body></Body>
 
     </div>
