3604998479e3393491323175497fcf393a2a044e
Aram32mm <a01657142@tec.mx>
2024-03-01 12:46:37 -0600

    falta funcionalidad de la search bar

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -3,56 +3,79 @@ import './InfoCard.css';
 import Tag from './Tags';
 
 interface InfoCardProps {
-  id: string;
-  imageUrl: string;
-  name: string;
-}
+    id: string;
+    imageUrl: string;
+    name: string;
+    experience: number;
+    height: number;
+    weight: number;
+    abilities: string[]; // Lista de habilidades
+    stats: {
+      hp: number;
+      attack: number;
+      defense: number;
+      specialAttack: number;
+      specialDefense: number;
+    };
+  }
+  
 
-const InfoCard: React.FC<InfoCardProps> = ({ id, imageUrl, name }) => {
-  return (
-    <div className="info__outter__frame">
-        <div className="info__centered__box">
-            <div className="info__circle">
-                <img className="info__img" src={imageUrl} alt="Card" />
-            </div>
-            <div className='info__box'>
-                <div className="info__frame">
-                    <div className="info__name">{name}</div>
-                </div>
-                <div className="grid grid-cols-3 px-28">
-                    <div className="info__subtitles">Experiencia</div>
-                    <div className="info__subtitles">Altura</div>
-                    <div className="info__subtitles">Peso</div>
-                </div>
-                <div className="grid grid-cols-3 px-28">
-                    <div className="info__txt">112</div>
-                    <div className="info__txt">4</div>
-                    <div className="info__txt">60</div>
-                </div>
-                <div className="info__subtitles px-28">Habilidades</div>
-                <div className="grid grid-flow-col auto-cols-max px-28 gap-4 mt-6">
-                    <Tag color="abilities" text="Static"></Tag>
-                    <Tag color="abilities" text="Lightning Rod"></Tag>
-                </div>
-                <div className="info__subtitles px-28">Stats</div>
-                <div className="grid grid-flow-col auto-cols-max gap-36 px-48 mt-6">
-                    <div className="info__stats__titles">HP</div>
-                        <div className="info__stats__titles">ATK</div>
-                        <div className="info__stats__titles">DEF</div>
-                        <div className="info__stats__titles">SPA</div>
-                        <div className="info__stats__titles">SPD</div>
-                    </div>
-                <div className="grid grid-flow-col auto-cols-max gap-4 px-28">
-                    <Tag color="hp" text="35"></Tag>
-                    <Tag color="atk" text="55"></Tag>
-                    <Tag color="def" text="40"></Tag>
-                    <Tag color="spa" text="50"></Tag>
-                    <Tag color="spd" text="50"></Tag>
-                </div>
-            </div>
-        </div>
-    </div>
-  );
-};
+const InfoCard: React.FC<InfoCardProps> = ({ 
+    id, 
+    imageUrl, 
+    name, 
+    experience, 
+    height, 
+    weight, 
+    abilities, 
+    stats 
+  }) => {
+    return (
+      <div className="info__outter__frame">
+          <div className="info__centered__box">
+              <div className="info__circle">
+                  <img className="info__img" src={imageUrl} alt={name} />
+              </div>
+              <div className='info__box'>
+                  <div className="info__frame">
+                      <div className="info__name">{name}</div>
+                  </div>
+                  <div className="grid grid-cols-3 px-28">
+                      <div className="info__subtitles">Experiencia</div>
+                      <div className="info__subtitles">Altura</div>
+                      <div className="info__subtitles">Peso</div>
+                  </div>
+                  <div className="grid grid-cols-3 px-28">
+                      <div className="info__txt">{experience}</div>
+                      <div className="info__txt">{height}</div>
+                      <div className="info__txt">{weight}</div>
+                  </div>
+                  <div className="info__subtitles px-28">Habilidades</div>
+                  <div className="grid grid-flow-col auto-cols-max px-28 gap-4 mt-6">
+                      {abilities.map((ability, index) => (
+                          <Tag key={index} color="abilities" text={ability}></Tag>
+                      ))}
+                  </div>
+                  <div className="info__subtitles px-28">Stats</div>
+                  <div className="grid grid-flow-col auto-cols-max gap-36 px-48 mt-6">
+                      <div className="info__stats__titles">HP</div>
+                      <div className="info__stats__titles">ATK</div>
+                      <div className="info__stats__titles">DEF</div>
+                      <div className="info__stats__titles">SPA</div>
+                      <div className="info__stats__titles">SPD</div>
+                  </div>
+                  <div className="grid grid-flow-col auto-cols-max gap-4 px-28">
+                      <Tag color="hp" text={stats.hp.toString()}></Tag>
+                      <Tag color="atk" text={stats.attack.toString()}></Tag>
+                      <Tag color="def" text={stats.defense.toString()}></Tag>
+                      <Tag color="spa" text={stats.specialAttack.toString()}></Tag>
+                      <Tag color="spd" text={stats.specialDefense.toString()}></Tag>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+  };
+  
 
 export default InfoCard;
--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -5,37 +5,69 @@ import InfoCard from './InfoCard';
 interface Pokemon {
     id: number;
     name: string;
-    image: string; // URL de la imagen del Pokémon
+    image: string; 
+    experience: number; 
+    height: number; 
+    weight: number; 
+    abilities: string[]; 
+    stats: {
+        hp: number;
+        attack: number;
+        defense: number;
+        specialAttack: number;
+        specialDefense: number;
+    };
 }
 
+
 export default function PokemonDetails() {
-  let { id } = useParams();
-  const [pokemon, setPokemon] = useState<Pokemon | null>(null); // Estado inicial como nulo, pero tipado como Pokemon o null
+    let { id } = useParams();
+    const [pokemon, setPokemon] = useState<Pokemon | null>(null); // Estado inicial como nulo, pero tipado como Pokemon o null
+  
+    useEffect(() => {
+        async function fetchPokemon() {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+            const data = await response.json();
+            setPokemon({
+                id: data.id,
+                name: data.name,
+                image: data.sprites.front_default,
+                experience: data.base_experience,
+                height: data.height,
+                weight: data.weight,
+                abilities: data.abilities.map((ability: { ability: { name: string }; }) => ability.ability.name),
+                stats: {
+                    hp: data.stats.find((stat: { stat: { name: string; }; }) => stat.stat.name === 'hp').base_stat,
+                    attack: data.stats.find((stat: { stat: { name: string; }; }) => stat.stat.name === 'attack').base_stat,
+                    defense: data.stats.find((stat: { stat: { name: string; }; }) => stat.stat.name === 'defense').base_stat,
+                    specialAttack: data.stats.find((stat: { stat: { name: string; }; }) => stat.stat.name === 'special-attack').base_stat,
+                    specialDefense: data.stats.find((stat: { stat: { name: string; }; }) => stat.stat.name === 'special-defense').base_stat,
+                }
+            });
+        }
 
-
-  useEffect(() => {
-    async function fetchPokemon() {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-      const data = await response.json();
-      setPokemon({
-        id: data.id,
-        name: data.name,
-        image: data.sprites.front_default
-      });
-    }
-
-    fetchPokemon();
-  }, [id]); // El useEffect se ejecuta cuando el ID cambia
-
-  return (
-    <div>
-      {pokemon ? (
+        fetchPokemon();
+    }, [id]);
+    
+    return (
         <div>
-          <InfoCard id={pokemon.id.toString()} imageUrl={pokemon.image} name={pokemon.name}></InfoCard>
+            {pokemon ? (
+                <div>
+                    <InfoCard
+                        id={pokemon.id.toString()}
+                        imageUrl={pokemon.image}
+                        name={pokemon.name}
+                        experience={pokemon.experience}
+                        height={pokemon.height}
+                        weight={pokemon.weight}
+                        abilities={pokemon.abilities}
+                        stats={pokemon.stats}
+                    ></InfoCard>
+                </div>
+            ) : (
+                <div>Cargando detalles del Pokémon...</div>
+            )}
         </div>
-      ) : (
-        <div>Cargando detalles del Pokémon...</div>
-      )}
-    </div>
-  );
+    );
+    
 }
