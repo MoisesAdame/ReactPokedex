bf0bd0e99cc38d9a824a9f90ee59b39f1c00f3fa
MoisesAdame <moisesadame01@gmail.com>
2024-03-01 21:30:57 -0600

    comments to PokeApi.tsx

--- src/api/PokeApi.tsx
+++ src/api/PokeApi.tsx
@@ -1,10 +1,17 @@
-import { api } from '../config/axiosConfig';
+// Importa el módulo 'api' desde la configuración de axios
+import { api } from '../config/axiosConfig'; 
 
 export const PokeApi = {
+  // Función asincrónica para obtener un Pokémon por su ID
   getPokemonById: async function (id: any) {
+    // Realiza una solicitud GET para obtener información del Pokémon 
+    // con el ID proporcionado
     return api.get(`/pokemon/${id}`);
   },
-  getPaginationPokemons: async function (limit = 20, offset = 0) {
-    return api.get(`/pokemon?limit=${limit}&offset=${offset}`);
+  // Función asincrónica para obtener una lista paginada de Pokémon
+  getPaginationPokemons: async function (limit = 20, offset = 0) { 
+    // Realiza una solicitud GET para obtener una lista paginada de 
+    // Pokémon con los límites y desplazamientos especificados
+    return api.get(`/pokemon?limit=${limit}&offset=${offset}`); 
   }
 };
